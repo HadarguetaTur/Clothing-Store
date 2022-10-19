763fc8aef10dbe5dff9e5fe2ab91ed970b1c92e2
hadar gueta <hadart20@gmail.com>
2022-10-19 23:43:06 +0300

    add cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -10,7 +10,7 @@ const CartDropdown = ()=>{
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                <div className='cart-items'>
+                <div className='cart-itemss'>
                     {cartItems.map(item=>
                     <CartItem key={item.id} cartItem={item}/>)}
                 </div>
